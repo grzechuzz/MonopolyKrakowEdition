abf2fd75082976eee069ede8a275c93364f9de1c
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-04-18 16:50:19 +0200

    Added constructor and sum field in RolledDouble class

--- src/main/java/model/jail/RolledDouble.java
+++ src/main/java/model/jail/RolledDouble.java
@@ -1,3 +1,14 @@
 package model.jail;
 
-public final class RolledDouble implements JailOutcome {}
+public final class RolledDouble implements JailOutcome {
+    private final int sum;
+
+    public RolledDouble(int sum) {
+        this.sum = sum;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+}
