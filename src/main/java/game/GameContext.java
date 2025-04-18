c13971bd586432816ae4f2e98bcc7f1fc26893e9
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-04-18 19:57:37 +0200

    Added services to GameContext class

--- src/main/java/game/GameContext.java
+++ src/main/java/game/GameContext.java
@@ -1,4 +1,44 @@
 package game;
 
+import service.*;
+
 public class GameContext {
+    private final PropertyTransactionService propertyTransactionService;
+    private final UserInteractionService userInteractionService;
+    private final TaxService taxService;
+    private final JailService jailService;
+    private final TravelService travelService;
+
+    public GameContext(PropertyTransactionService propertyTransactionService,
+                       UserInteractionService userInteractionService,
+                       TaxService taxService,
+                       JailService jailService,
+                       TravelService travelService)
+    {
+        this.propertyTransactionService = propertyTransactionService;
+        this.userInteractionService = userInteractionService;
+        this.taxService = taxService;
+        this.jailService = jailService;
+        this.travelService = travelService;
+    }
+
+    public PropertyTransactionService getPropertyTransactionService() {
+        return propertyTransactionService;
+    }
+
+    public UserInteractionService getUserInteractionService() {
+        return userInteractionService;
+    }
+
+    public TaxService getTaxService() {
+        return taxService;
+    }
+
+    public JailService getJailService() {
+        return jailService;
+    }
+
+    public TravelService getTravelService() {
+        return travelService;
+    }
 }
