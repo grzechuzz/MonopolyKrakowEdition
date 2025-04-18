b8ee998bad3f022bb3420a80e25e408fc85fb9d9
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-04-18 20:41:03 +0200

    Applied Builder design pattern in GameContext class

--- src/main/java/game/GameContext.java
+++ src/main/java/game/GameContext.java
@@ -8,26 +8,74 @@ public class GameContext {
     private final TaxService taxService;
     private final JailService jailService;
     private final TravelService travelService;
+    private final CardService cardService;
+    private final FestivalService festivalService;
 
-    public GameContext(PropertyTransactionService propertyTransactionService,
-                       UserInteractionService userInteractionService,
-                       TaxService taxService,
-                       JailService jailService,
-                       TravelService travelService)
-    {
-        this.propertyTransactionService = propertyTransactionService;
-        this.userInteractionService = userInteractionService;
-        this.taxService = taxService;
-        this.jailService = jailService;
-        this.travelService = travelService;
+    private GameContext(Builder b) {
+        this.propertyTransactionService = b.propertyTransactionService;
+        this.userInteractionService = b.userInteractionService;
+        this.taxService = b.taxService;
+        this.jailService = b.jailService;
+        this.travelService = b.travelService;
+        this.cardService = b.cardService;
+        this.festivalService = b.festivalService;
     }
 
-    public PropertyTransactionService getPropertyTransactionService() {
-        return propertyTransactionService;
+    public static class Builder {
+        private PropertyTransactionService propertyTransactionService;
+        private UserInteractionService userInteractionService;
+        private TaxService taxService;
+        private JailService jailService;
+        private TravelService travelService;
+        private CardService cardService;
+        private FestivalService festivalService;
+
+        public Builder pts(PropertyTransactionService pts) {
+            propertyTransactionService = pts;
+            return this;
+        }
+
+        public Builder ui(UserInteractionService ui) {
+            userInteractionService = ui;
+            return this;
+        }
+
+        public Builder tax(TaxService ts) {
+            taxService = ts;
+            return this;
+        }
+
+        public Builder jail(JailService js) {
+            jailService = js;
+            return this;
+        }
+
+        public Builder travel(TravelService ts) {
+            travelService = ts;
+            return this;
+        }
+
+        public Builder card(CardService cs) {
+            cardService = cs;
+            return this;
+        }
+
+        public Builder festival(FestivalService fs) {
+            festivalService = fs;
+            return this;
+        }
+
+        public GameContext build() {
+            return new GameContext(this);
+        }
     }
 
     public UserInteractionService getUserInteractionService() {
         return userInteractionService;
+    }
+
+    public PropertyTransactionService getPropertyTransactionService() {
+        return propertyTransactionService;
     }
 
     public TaxService getTaxService() {
@@ -40,5 +88,13 @@ public class GameContext {
 
     public TravelService getTravelService() {
         return travelService;
+    }
+
+    public CardService getCardService() {
+        return cardService;
+    }
+
+    public FestivalService getFestivalService() {
+        return festivalService;
     }
 }
