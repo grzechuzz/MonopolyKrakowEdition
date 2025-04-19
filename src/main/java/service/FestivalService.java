9c1fa7f328ca47616e56436c839cec98cab3e978
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-04-19 02:06:36 +0200

    Developed FestivalService class

--- src/main/java/service/FestivalService.java
+++ src/main/java/service/FestivalService.java
@@ -1,4 +1,30 @@
 package service;
 
+import model.board.Board;
+import model.field.Field;
+import model.field.PropertyField;
+import model.player.Player;
+
 public class FestivalService {
+    private final UserInteractionService ui;
+    private PropertyField activeFestival;
+
+    public FestivalService(UserInteractionService ui, Board board) {
+        this.ui = ui;
+    }
+
+    public boolean startFestival(Player player) {
+        PropertyField field = ui.chooseFestivalField(player);
+        if (field == null)
+            return false;
+
+        if (activeFestival != null)
+            activeFestival.setFestivalActive(false);
+
+        field.setFestivalActive(true);
+        activeFestival = field;
+        ui.displayMessage("Festiwal na polu " + field.getName() +". Czynsz x2!");
+
+        return true;
+    }
 }
