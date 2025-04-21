af4e948a75ee32a9c9dbf7296719d1ccc9d7fbd8
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-04-21 03:43:52 +0200

    Developed GameEngine class - player turns, game ending etc.

---
+++ src/main/java/game/GameEngine.java
@@ -0,0 +1,43 @@
+package game;
+
+import model.player.Player;
+
+import java.util.List;
+
+public class GameEngine {
+    private final GameContext gameContext;
+    private final TurnHandler turnHandler;
+    private final VictoryChecker victoryChecker;
+    private int currentPlayer = 0;
+
+    public GameEngine(GameContext gameContext, TurnHandler turnHandler, VictoryChecker victoryChecker) {
+        this.gameContext = gameContext;
+        this.turnHandler = turnHandler;
+        this.victoryChecker = victoryChecker;
+    }
+
+    public void runGame() {
+        List<Player> players = gameContext.getPlayers();
+        while (true) {
+            Player p = players.get(currentPlayer);
+
+            if (!p.getStatus().isEliminated()) {
+                turnHandler.executeTurn(p);
+                if (p.getStatus().isEliminated()) {
+                    players.remove(currentPlayer);
+                } else {
+                    if (victoryChecker.checkVictory(p, players)) {
+                        break;
+                    } else if (p.getStatus().getConsecutiveDoubles() == 0) {
+                        currentPlayer = (currentPlayer + 1) % players.size();
+                    }
+                }
+            } else {
+                players.remove(currentPlayer);
+            }
+        }
+        Player winner = players.get(0);
+        gameContext.getUserInteractionService().displayMessage("Koniec gry! Zwycięzca: " + winner.getNickname());
+
+    }
+}
