fb54fd383011015a1b1682f23eed4aede6e743de
grzechuzz <grzechuzz@users.noreply.github.com>
2025-04-21 17:02:45 +0200

    Developed CardService - responsible for drawing and keeping chance cards.

--- src/main/java/service/CardService.java
+++ src/main/java/service/CardService.java
@@ -1,5 +1,21 @@
 package service;
 
-public class CardService {
+import model.chance.ChanceCard;
 
+import java.util.List;
+import java.util.Random;
+
+public class CardService {
+    private final List<ChanceCard> cards;
+    private final Random random;
+
+    public CardService(List<ChanceCard> cards, Random random) {
+        this.cards = cards;
+        this.random = random;
+    }
+
+    public ChanceCard draw() {
+        int idx = random.nextInt(cards.size());
+        return cards.get(idx);
+    }
 }
