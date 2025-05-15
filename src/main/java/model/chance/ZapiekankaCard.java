d8800be62ad74c52b0ead75ea7778c9ef867e3cc
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-05-15 15:23:36 +0200

    Added new ChanceCard - ZapiekankaCard

---
+++ src/main/java/model/chance/ZapiekankaCard.java
@@ -0,0 +1,18 @@
+package model.chance;
+
+import game.GameContext;
+import model.player.Player;
+
+public class ZapiekankaCard implements ChanceCard {
+    @Override
+    public String getDescription() {
+        return "Jesteś bardzo głodny i masz ochotę na zapiekankę. Idziesz na pole Okrąglak. Dodatkowo masz dziś bardzo" +
+                "dobry humor, więc stawiasz zapiekanki pozostałym graczom - łącznie za 200 000 PLN.";
+    }
+
+    @Override
+    public void apply(Player player, GameContext gc) {
+        gc.getMovementService().moveTo(player, 37);
+        gc.getPropertyTransactionService().payToBank(player, 200000);
+    }
+}
