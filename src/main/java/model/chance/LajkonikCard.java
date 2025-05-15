ec05a1d7292731b5b69e5a7fe1403d9ff15bab37
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-05-15 15:50:58 +0200

    Added new ChanceCard - LajkonikCard

---
+++ src/main/java/model/chance/LajkonikCard.java
@@ -0,0 +1,18 @@
+package model.chance;
+
+import game.GameContext;
+import model.player.Player;
+import utils.Rules;
+
+public class LajkonikCard implements ChanceCard {
+    @Override
+    public String getDescription() {
+        return "Zostałes uderzony buławą przez Lajkonika podczas pochodu! Przynosi Ci to szczęscie -" +
+                " otrzymujesz 250 000 PLN!";
+    }
+
+    @Override
+    public void apply(Player player, GameContext gc) {
+        player.addBalance(Rules.CARD_LAJKONIK_REWARD);
+    }
+}
